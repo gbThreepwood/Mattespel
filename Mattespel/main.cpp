886be93dcbfa94e9ee11f2d45f218d9e301d9cd8
eirik <eirik@la2kta.net>
2013-01-20 13:50:18 +0100

    Diverse forbetringar: * Tilfeldige regneartar. * Betre handering av poengfil.

--- Mattespel/main.cpp
+++ Mattespel/main.cpp
@@ -1,47 +1,111 @@
-/*
+/*********************************************
+ * vim:sw=8:ts=8:si:et
+ * To use the above modeline in vim you must have "set modeline" in your .vimrc
+ *
  * main.cpp
  *
  *  Created on: 14. jan. 2013
- *      Author: eirik
- */
+ *
+ * Author: Eirik
+ *
+ * Copyright:GPL V3
+ * See http://www.gnu.org/licenses/gpl.html
+ *
+ * Eit enkelt mattespel for å demonstrera C++.
+ *********************************************/
 
+// Standard C++ bibliotek
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 #include <fstream>
+#include <string>
+
+// Gettext, for omsetjing
+#include <libintl.h>
+#include <locale>
+
+// Ncurses, for litt meir fancy grafikk.
+#include <ncurses.h>
+#include <menu.h>
+
+#include "main.h"
 
 
-//Definer funksjonane
-int multiplikasjon();
-int lagre_poengsum(int poengsum);
 
 int main(int argc, char *argv[])
 {
+	if ( argc != 2 ) // argc må vera 2
+		std::cout <<"usage: "<< argv[0] <<" <ant oppg>\n";
 
+	// Set opp gettext
+	setlocale( LC_ALL, "" );
+	bindtextdomain( "hello", "/usr/share/locale" );
+	textdomain( "hello" );
 
-	std::cout << "Velkomen til Mattespel v1.0" << std::endl;
+	// gjeldande tid og dato, basert på systemtid.
+	time_t now = time(0);
 
-	lagre_poengsum(multiplikasjon());
+	// konverter til streng
+	char* dt = ctime(&now);
+
+	std::cout << _("Welcome to Mattespel v1.0") << std::endl;
+	std::cout << "Lokal tid: " << dt << std::endl;
+
+	// Storleiken til forskjellige variabeltypar
+	std::cout << "Short: " << sizeof(short) << std::endl;
+	std::cout << "Int: " << sizeof(int) << std::endl;
+	std::cout << "Long: " << sizeof(long) << std::endl;
+	std::cout << "Char: " << sizeof(char) << std::endl;
+
+	// Multiplikasjon
+	lagre_poengsum(tilfeldigOppg(4));
 
 	return 0;
 }
 
-int multiplikasjon()
+int tilfeldigOppg(int ant_oppg)
 {
 	int poengsum = 0;
+	int svar = 0;
+	int fasit = 0;
+	int tilfeldig1 = 0;
+	int tilfeldig2 = 0;
+	enum regneart {multiplikasjon, divisjon, subtraksjon, addisjon, last};
 
+	// Initialiser tilfeldighetsgeneratoren basert på klokka.
 	srand((unsigned)time(0));
 
-
-	for(int i = 0; i < 2; i++)
+	for(int i = 0; i < ant_oppg; i++)
 	{
-		int tilfeldig1 = (rand()%99) + 1;
-		int tilfeldig2 = (rand()%99) + 1;
-		int svar = 0;
+		// Generer tilfeldige tal
+		tilfeldig1 = (rand()%9) + 1;
+		tilfeldig2 = (rand()%9) + 1;
 
-		int fasit = tilfeldig1*tilfeldig2;
+		//Velg ein tilfeldig regneart
+		regneart oppgType = static_cast<regneart>(rand()%last);
 
-		std::cout << tilfeldig1 << " * " << tilfeldig2 << " = ";
+		if (oppgType == multiplikasjon)
+		{
+			fasit = tilfeldig1*tilfeldig2;
+			std::cout << tilfeldig1 << " * " << tilfeldig2 << " = ";
+		}
+		else if (oppgType == divisjon)
+		{
+			fasit = tilfeldig1/tilfeldig2;
+			std::cout << tilfeldig1 << " / " << tilfeldig2 << " = ";
+		}
+		else if (oppgType == subtraksjon)
+		{
+			fasit = tilfeldig1-tilfeldig2;
+			std::cout << tilfeldig1 << " - " << tilfeldig2 << " = ";
+		}
+		else
+		{
+			fasit = tilfeldig1+tilfeldig2;
+			std::cout << tilfeldig1 << " + " << tilfeldig2 << " = ";
+		}
+
 		std::cin >> svar;
 
 		if(fasit == svar)
@@ -61,11 +125,13 @@ int multiplikasjon()
 
 int lagre_poengsum(int poengsum)
 {
-	char namn[] = "";
+	//char namn[] = "";
+	std::string namn = "";
 
 	std::cout << "Gratulera du fekk: " << poengsum << " poeng." << std::endl;
 	std::cout << "Skriv namnet ditt: ";
-	std::cin >> namn;
+	std::getline(std::cin, namn);
+	std::cout << std::endl << "Lagrer poengsum . . .";
 
 	std::ofstream poengfil;
 
---
+++ Mattespel/main.h
@@ -0,0 +1,20 @@
+/*
+ * main.h
+ *
+ *  Created on: 20. jan. 2013
+ *      Author: eirik
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+//Definisjonar
+#define _(STRING)    gettext(STRING)
+
+//Definer funksjonane
+int tilfeldigOppg(int ant_oppg);
+int lagre_poengsum(int poengsum);
+
+
+
+#endif /* MAIN_H_ */
